d08ab6c53286eef85271c1c1fdb7d2781b154b0d
serge487 <sergekahwaji1@gmail.com>
2026-02-27 12:39:51 +0200

    feat : complete admin panel

---
+++ laravel/app/Filament/Resources/MenuItems/Schemas/MenuItemForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Resources\MenuItems\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class MenuItemForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('category_id')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('$'),
+                FileUpload::make('image')
+                    ->image(),
+                Toggle::make('is_available')
+                    ->required(),
+            ]);
+    }
+}
