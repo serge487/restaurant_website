8845da6319404f4a00945b5fddfbe26290cc1f09
serge487 <sergekahwaji1@gmail.com>
2026-03-04 16:42:08 +0200

    changed some ui

--- laravel/app/Providers/Filament/AdminPanelProvider.php
+++ laravel/app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('Admin Control Panel')
             ->colors([
                 'primary' => Color::Amber,
             ])
@@ -38,8 +39,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-                AccountWidget::class,
-                FilamentInfoWidget::class,
+                AccountWidget::class
             ])
             ->middleware([
                 EncryptCookies::class,
